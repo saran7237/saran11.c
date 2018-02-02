c09e91456372a7037101edfe139a05b5998e2fee
saran7237 <36067371+saran7237@users.noreply.github.com>
2018-02-02 14:02:33 +0530

    Create saran11.c

---
+++ saran11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+clrscr();
+printf("Enter the number"a);
+scanf("%d",&a);
+if(a>0)
+printf("The number is positive",b);
+else if(a<0)
+printf("The number is negative",c);
+else
+printf("zero");
+  getch();
+  return 0;
+}
