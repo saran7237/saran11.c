4f3ba05dc0308fba9847ebb71586450ea984ba94
saran7237 <36067371+saran7237@users.noreply.github.com>
2018-02-02 14:26:20 +0530

    Create saran15.c

---
+++ saran15.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int d,e,f;
+clrscr();
+scan("%d%d%d,&a&d&f")
+printf("enter the numbers");
+if ((d>e)&&(d>f))
+printf("d is greater");
+else if
+printf("e is greater");
+else
+printf("f is greater");
+getch();
+return 0;
+}
